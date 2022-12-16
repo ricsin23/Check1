cdcceae16cb99abd6dcb33de814c5ebab7b4762f
richa.singh <richa.singh@DESKTOP-3QI9VE2>
2022-12-16 15:51:16 +0530

    Add project files.

---
+++ Website/Controllers/WebsiteController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Website.Controllers
+{
+	public class WebsiteController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+		public IActionResult About()
+		{
+			return View();
+		}
+		public IActionResult Courses()
+		{
+			return View();
+		}
+	}
+}
---
+++ Website/Models/Course.cs
@@ -0,0 +1,8 @@
+﻿namespace Website.Models
+{
+	public class Course
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+	}
+}
